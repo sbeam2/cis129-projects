794dd7d8a63355df77c62cfe849385b77944ff56
sbeam2 <sbeam2@mail.pima.edu>
2024-09-10 10:35:21 -0700

    Update cis129_lab02_shortStory.py New story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print("Hello world")
+#Author: Saam Beam
+#My first Python lab in my CIS129 course
+#Prints a simple short story
+
+print("John was walking down the street")
+print("He got hungry")
+print("So he went into a store")
+print("He bought some chips")
+print("John ate the chips")
