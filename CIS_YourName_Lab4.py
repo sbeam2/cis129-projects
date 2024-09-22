a381b50e84ef3b7258f4b07924dc28fe598313a3
sbeam2 <sbeam2@mail.pima.edu>
2024-09-22 12:14:31 -0700

    Add files via upload

---
+++ CIS_YourName_Lab4.py
@@ -0,0 +1,53 @@
+# Module 4 Lab-4
+# Saam Beam
+# 9/18/2024
+# Show amount of bonus money based off of other variables 
+
+# declare local variables
+monthlySales = 0  # monthly sales amount
+storeAmount = 0  # store bonus amount
+empAmount = 0  # employee bonus amount
+salesIncrease = 0  # percent of sales increase
+prompt = ("enter monthly sales ") # prompt will be a string literal
+
+    
+
+# This code gets the monthly sales
+
+monthlySales = float(input(prompt))
+
+# This code determines the store bonus
+
+if monthlySales >= 110000:
+	storeAmount = 6000
+elif monthlySales >= 100000:
+	storeAmount = 5000
+elif monthlySales >= 90000:
+	storeAmount = 4000
+elif monthlySales >= 80000:
+	storeAmount = 3000
+else:
+	storeAmount = 0
+
+
+
+# This code gets the percent of increase in sales
+salesIncrease = float(input("Enter percent of sales increase "))
+salesIncrease = salesIncrease / 100
+
+
+# This code determines the employee bonus
+if salesIncrease >= .05:
+	empAmount = 75
+elif salesIncrease >= .04:
+	empAmount = 50
+elif salesIncrease >= .03:
+	empAmount = 40
+else:
+	empAmount = 0
+
+# This code prints the bonus information
+print("The store bonus amount is $", storeAmount)
+print("The employee bonus amount is $", empAmount)
+if (storeAmount == 6000 ) and (empAmount == 75):
+	print('Congrats! You have reached the highest bonus amounts possible! ')
